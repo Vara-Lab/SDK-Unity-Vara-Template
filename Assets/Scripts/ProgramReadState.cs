1b4a191f0fd602660e4f32398493a20198c8e5d1
A-mont <a.mont.24.777@gmail.com>
2024-08-27 14:12:13 -0600

    Docs: Update ProgramReadState.cs Example

--- Assets/Scripts/ProgramReadState.cs
+++ Assets/Scripts/ProgramReadState.cs
@@ -20,7 +20,7 @@ public class ProgramReadState : MonoBehaviour
 
 
     // The sender's address in hexadecimal format, which will be used to sign the transaction or query.
-    string sender = "0x......";
+    string sender = "0xe4fa3b466792dcd7e58f5d8d49bc4631b5eec3a9ebe48ffe79f859dadf76cb71";
 
     // The program ID corresponding to the deployed contract on the Vara blockchain.
     string programId = "0x9b70e858462eedf89896808af3082673b740404961c118109a2217536c907f5a";
@@ -32,37 +32,10 @@ public class ProgramReadState : MonoBehaviour
     // Set the value being sent along with the transaction. For this query, no value is transferred, so it's set to zero.
     int value = 0;
 
-    // Define the Interface Description Language (IDL) for the smart contract.
-    // The IDL describes the structure of types and services provided by the contract, which helps in decoding the results of queries and calls.
-    string idl = @"
-            type IoTrafficLightState = struct {
-              current_light: str,
-              all_users: vec struct { actor_id, str },
-            };
-
-            constructor {
-              New : ();
-            };
-
-            service TrafficLight {
-              Green : () -> TrafficLightEvent;
-              Red : () -> TrafficLightEvent;
-              Yellow : () -> TrafficLightEvent;
-              query Current : () -> str;
-              query TrafficLightState : () -> IoTrafficLightState;
-              query Users : () -> vec struct { actor_id, str };
-
-              events {
-                Green;
-                Yellow;
-                Red;
-              }
-            };";
-
     // Asynchronously query the state of the smart contract using the QueryService.
     // The query will return the current state as described in the IDL.
     object result = await QueryService.QueryStateAsync(
-        url, idl, service, query, type, sender, programId, gasLimit, value, payload: null
+        url, service, query, type, sender, programId, gasLimit, value, payload: null
     );
 
     
